6af94d6eb15d3d9b1de388131e32b39a469f5a56
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2022-02-24 16:17:03 +0530

    Add Sidebar

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+const sidebar = document.querySelector('.sidebar-container')
+const toggleBtn = document.querySelector('.sidebar__toggle')
+
+
+toggleBtn.addEventListener('click', ()=>{
+	sidebar.classList.toggle('close')
+})
