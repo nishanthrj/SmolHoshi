c0f10c6fce880a3f33e8867befd7fa4675be9ab7
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2022-03-01 19:50:08 +0530

    Fix sidebar position

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 const sidebar = document.querySelector(".sidebar-container");
 const toggleBtn = document.querySelector(".sidebar__toggle");
-
+const sidebarSpace = document.querySelector(".sidebar-space");
 
 toggleBtn.addEventListener("click", () => {
 	sidebar.classList.toggle("mini");
+	sidebarSpace.classList.toggle("mini");
 });
-
