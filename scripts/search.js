938ce1fa4fe3ffea89497c959b7525383f68032d
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2022-03-06 20:08:09 +0530

    Refactor search page

--- scripts/search.js
+++ scripts/search.js
@@ -1,7 +1,7 @@
 const mediaDropdownBtn = document.querySelector(".media-type");
 const mediaDropdown = document.querySelector(".type__dropdown__list");
 
-const filterFields = document.querySelector(".filter-field-container");
+const filterFields = document.querySelector(".filter__field__container");
 
 const genreDropdown = document.querySelector(".genres__list");
 const genreBox = document.querySelector("#genre-value");
@@ -15,9 +15,9 @@ const statusDropdown = document.querySelector(".status__list");
 const statusBox = document.querySelector("#status-value");
 let selectedStatus;
 
-const sortBtn = document.querySelector(".active__sort__method");
+const sortBtn = document.querySelector(".sort__method");
 const sortDropdown = document.querySelector(".sort__list");
-const activeSortTitle = document.querySelector(".active__sort__method");
+const activeSortTitle = document.querySelector(".sort__method");
 
 const activeFilterContainer = document.querySelector(".active__filter-wrap");
 const query = document.querySelector("#query");
@@ -26,129 +26,7 @@ let mediaType = mediaDropdownBtn.textContent.toLowerCase();
 let totalPages;
 let page = 1;
 
-mediaDropdownBtn.addEventListener("click", () => {
-	const dropdown = document.querySelector(".type__dropdown");
-	let dropdownIcon = mediaDropdownBtn.firstElementChild;
-	_closeDropdown(dropdown);
-	_flipIcon(dropdownIcon);
-	dropdown.classList.toggle("open-dropdown");
-});
 
-filterFields.addEventListener("click", (e) => {
-	const filterBox = e.target.closest(".filter");
-	const dropdown = e.target.closest(".filter").querySelector(".filter__dropdown");
-	let dropdownIcon = filterBox.querySelector(".drop-icon");
-
-	if (!dropdown.contains(e.target)) {
-		_closeDropdown(dropdown);
-		_flipIcon(dropdownIcon);
-
-		dropdown.classList.toggle("open-dropdown");
-	}
-});
-
-sortBtn.addEventListener("click", (e) => {
-	const dropdown = document.querySelector(".sort__dropdown");
-	if (!dropdown.contains(e.target)) {
-		_closeDropdown(dropdown);
-		dropdown.classList.toggle("open-dropdown");
-	}
-});
-
-mediaDropdown.addEventListener("click", (e) => {
-	if (e.target.classList.contains("type__dropdown__option")) {
-		mediaDropdownBtn.innerHTML = `${e.target.textContent}<i class="fi fi-rs-angle-small-down media-type__icon"></i>`;
-		mediaType = e.target.textContent.toLowerCase();
-		reset();
-		_closeDropdown();
-		switchType();
-	}
-});
-
-genreDropdown.addEventListener("click", (e) => {
-	if (e.target.classList.contains("filter__dropdown__option")) {
-		if (!e.target.classList.contains("checked")) {
-			e.target.insertAdjacentHTML("beforeend", '<i class="fi fi-rs-check"></i>');
-			selectedGenres.add(e.target.textContent);
-		} else {
-			e.target.querySelector("i").remove();
-			selectedGenres.delete(e.target.textContent);
-		}
-		e.target.classList.toggle("checked");
-		_showMultiTags(genreBox, selectedGenres);
-		_activeFilters();
-		_generateMediaCard();
-	}
-});
-
-typeDropdown.addEventListener("click", (e) => {
-	if (e.target.classList.contains("filter__dropdown__option")) {
-		if (!e.target.classList.contains("checked")) {
-			if (selectedType) {
-				selectedType.querySelector("i").remove();
-				selectedType.classList.remove("checked");
-			}
-			selectedType = e.target;
-			selectedType.insertAdjacentHTML("beforeend", '<i class="fi fi-rs-check"></i>');
-			selectedType.classList.add("checked");
-			typeBox.querySelector(".placeholder").innerText = selectedType.textContent;
-		} else {
-			e.target.querySelector("i").remove();
-			e.target.classList.remove("checked");
-			selectedType = null;
-			typeBox.querySelector(".placeholder").innerText = "Any";
-		}
-		_closeDropdown();
-		_activeFilters();
-		_generateMediaCard();
-	}
-});
-
-statusDropdown.addEventListener("click", (e) => {
-	if (e.target.classList.contains("filter__dropdown__option")) {
-		if (!e.target.classList.contains("checked")) {
-			if (selectedStatus) {
-				selectedStatus.querySelector("i").remove();
-				selectedStatus.classList.remove("checked");
-			}
-			selectedStatus = e.target;
-			selectedStatus.insertAdjacentHTML("beforeend", '<i class="fi fi-rs-check"></i>');
-			selectedStatus.classList.add("checked");
-			statusBox.querySelector(".placeholder").innerText = selectedStatus.textContent;
-		} else {
-			e.target.querySelector("i").remove();
-			e.target.classList.remove("checked");
-			selectedStatus = null;
-			statusBox.querySelector(".placeholder").innerText = "Any";
-		}
-		_closeDropdown();
-		_activeFilters();
-		_generateMediaCard();
-	}
-});
-
-sortDropdown.addEventListener("click", (e) => {
-	if (e.target.classList.contains("sort__dropdown__option")) {
-		activeSortTitle.dataset.sort = e.target.dataset.sort;
-		activeSortTitle.innerHTML = `<i class="fi fi-rs-sort"></i>${e.target.textContent}`;
-		_closeDropdown();
-		_generateMediaCard();
-	}
-});
-
-query.addEventListener("input", () => {
-	_activeFilters();
-	if (query.value.length >= 3) _generateMediaCard();
-});
-
-const loadMedia = function (entries, observer) {
-	if (entries[0].isIntersecting) {
-		observer.unobserve(entries[0].target);
-		if (page <= totalPages) page += 1;
-		_generateMediaCard((load = true));
-	}
-};
-const observer = new IntersectionObserver(loadMedia, { threshold: [0.5] });
 
 const reset = function () {
 	totalPages = 0;
@@ -160,20 +38,20 @@ const reset = function () {
 	document.querySelector(".results").innerHTML = "";
 	activeFilterContainer.innerHTML = "";
 	query.value = "";
-	activeSortTitle.textContent = "Popularity";
-	genreBox.innerHTML = `<p class="placeholder">Any</p>
+	activeSortTitle.innerHTML = `<i class="fi fi-rs-sort"></i>Popularity`;
+	genreBox.innerHTML = `<p class="field__placeholder">Any</p>
 		<div class="tag-wrap">
 			<span class="tag first-tag"></span>
 			<span class="tag other-tag"></span>
 		</div>
 		<i class="fi fi-rs-angle-small-down drop-icon"></i>`;
-	typeBox.querySelector(".placeholder").innerHTML = "Any";
-	statusBox.querySelector(".placeholder").innerHTML = "Any";
+	typeBox.querySelector(".field__placeholder").innerHTML = "Any";
+	statusBox.querySelector(".field__placeholder").innerHTML = "Any";
 	const dropdowns = [...document.querySelectorAll(".filter__dropdown__list")];
 	dropdowns.forEach((d) => {
 		[...d.children].forEach((opt) => {
-			if (opt.classList.contains("checked")) {
-				opt.classList.remove("checked");
+			if (opt.classList.contains("filter__dropdown__option--selected")) {
+				opt.classList.remove("filter__dropdown__option--selected");
 				opt.querySelector(".fi").remove();
 			}
 		});
@@ -212,45 +90,45 @@ const switchType = function () {
 	}
 };
 
-const _flipIcon = function (icon) {
+const flipIcon = function (icon) {
 	if (icon.className.includes("up")) icon.className = icon.className.replace("up", "down");
 	else icon.className = icon.className.replace("down", "up");
 };
 
-const _closeDropdown = function (ignore) {
+const closeDropdown = function (ignore) {
 	const dropdowns = document.querySelectorAll(".dropdown");
 	dropdowns.forEach((d) => {
-		if (d.classList.contains("open-dropdown") && d !== ignore)
-			d.classList.remove("open-dropdown");
+		if (d.classList.contains("dropdown--open") && d !== ignore)
+			d.classList.remove("dropdown--open");
 	});
 };
 
-const _showMultiTags = function (container, elements) {
+const showMultiTags = function (container, elements) {
 	if (elements.size !== 0) {
 		let selected = [...elements];
-		container.querySelector(".placeholder").classList.add("hide");
-		container.querySelector(".tag-wrap").classList.add("show");
-		container.querySelector(".first-tag").classList.add("show");
+		container.querySelector(".field__placeholder").classList.add("field__placeholder--hide");
+		container.querySelector(".tag-wrap").classList.add("tag-wrap--show");
+		container.querySelector(".first-tag").classList.add("tag--show");
 		container.querySelector(".first-tag").innerHTML = selected[0];
 		if (elements.size > 1) {
-			container.querySelector(".other-tag").classList.add("show");
+			container.querySelector(".other-tag").classList.add("tag--show");
 			container.querySelector(".other-tag").innerHTML = `+${elements.size - 1}`;
 		} else {
-			container.querySelector(".other-tag").classList.remove("show");
+			container.querySelector(".other-tag").classList.remove("tag--show");
 			container.querySelector(".other-tag").innerHTML = "";
 		}
 	} else {
-		container.querySelector(".placeholder").classList.remove("hide");
-		container.querySelector(".tag-wrap").classList.remove("show");
-		container.querySelector(".first-tag").classList.remove("show");
+		container.querySelector(".field__placeholder").classList.remove("field__placeholder--hide");
+		container.querySelector(".tag-wrap").classList.remove("tag-wrap--show");
+		container.querySelector(".first-tag").classList.remove("tag--show");
 		container.querySelector(".first-tag").innerHTML = "";
 	}
 };
 
-const _getUserQuery = function () {
-	const genres = genreDropdown.querySelectorAll(".checked");
-	const type = typeDropdown.querySelector(".checked");
-	const status = statusDropdown.querySelector(".checked");
+const getUserQuery = function () {
+	const genres = genreDropdown.querySelectorAll(".filter__dropdown__option--selected");
+	const type = typeDropdown.querySelector(".filter__dropdown__option--selected");
+	const status = statusDropdown.querySelector(".filter__dropdown__option--selected");
 
 	return {
 		q: query.value ? query.value : "",
@@ -262,19 +140,19 @@ const _getUserQuery = function () {
 	};
 };
 
-const _activeFilters = function () {
+const activeFilters = function () {
 	let activeFilters = "";
-	const filters = _getUserQuery();
-	const genresNames = genreDropdown.querySelectorAll(".checked");
+	const filters = getUserQuery();
+	const genresNames = genreDropdown.querySelectorAll(".filter__dropdown__option--selected");
 
-	if (filters.q) activeFilters += `<div class="active__filter">Search: ${filters.q}</div>\n`;
+	if (filters.q) activeFilters += `<div class="active__filter__item">Search: ${filters.q}</div>\n`;
 	if (genresNames.length > 0) {
 		genresNames.forEach((g) => {
-			activeFilters += `<div class="active__filter">${g.textContent}</div>\n`;
+			activeFilters += `<div class="active__filter__item">${g.textContent}</div>\n`;
 		});
 	}
-	if (filters.type) activeFilters += `<div class="active__filter">${filters.type}</div>\n`;
-	if (filters.status) activeFilters += `<div class="active__filter">${filters.status}</div>\n`;
+	if (filters.type) activeFilters += `<div class="active__filter__item">${filters.type}</div>\n`;
+	if (filters.status) activeFilters += `<div class="active__filter__item">${filters.status}</div>\n`;
 
 	if (activeFilters) {
 		activeFilterContainer.innerHTML = activeFilters;
@@ -283,14 +161,14 @@ const _activeFilters = function () {
 	}
 };
 
-const _ratingIcon = function (rating) {
+const ratingIcon = function (rating) {
 	if (!rating) return "";
 	else if (rating <= 4) return `<i class="fi fi-rs-sad"></i>`;
 	else if (rating >= 7) return `<i class="fi fi-rs-smile"></i>`;
 	else return `<i class="fi fi-rs-meh"></i>`;
 };
 
-const _generateGenreElements = function (genres) {
+const generateGenreElements = function (genres) {
 	let tags = "";
 	genres.slice(0, 5).forEach((g) => {
 		tags += `<span class="media__card__info__genre">${g.name}</span>\n`;
@@ -298,14 +176,14 @@ const _generateGenreElements = function (genres) {
 	return tags;
 };
 
-const _formatScoreInfo = function (media) {
+const formatScoreInfo = function (media) {
 	if (mediaType === "manga") {
 		return media.scored;
 	}
 	return media.score;
 };
 
-const _formatExtraInfo = function (media) {
+const formatExtraInfo = function (media) {
 	if (mediaType === "manga") {
 		return (
 			`${media.chapters ? " • " + media.chapters + " Chapter" : ""}` +
@@ -318,11 +196,11 @@ const _formatExtraInfo = function (media) {
 	);
 };
 
-const _generateMediaCard = function (load = false) {
+const generateMediaCard = function (load = false) {
 	setTimeout(() => {
 		let cards = "";
 		const container = document.querySelector(".results");
-		const query = _getUserQuery();
+		const query = getUserQuery();
 		const url =
 			`https://api.jikan.moe/v4/${mediaType}?q=${query.q}&page=${page}&` +
 			`type=${query.type}&status=${query.status}&sfw=true&genres_exclude=49` +
@@ -340,16 +218,16 @@ const _generateMediaCard = function (load = false) {
 								<div class="media__card__info">
 									<div class="media__card__info__header-wrap">
 										<a href="#" class="media__card__info__title">${media.title}</a>
-										<p class="media__card__info__score">${_ratingIcon(_formatScoreInfo(media))}
-										${_formatScoreInfo(media) ? _formatScoreInfo(media).toString().padEnd(4, 0) : ""}</p>
+										<p class="media__card__info__score">${ratingIcon(formatScoreInfo(media))}
+										${formatScoreInfo(media) ? formatScoreInfo(media).toString().padEnd(4, 0) : ""}</p>
 									</div>
 									<div class="media__card__info__extra-wrap">
 										<p class="media__card__info__extra">
 										${media.type}
-										${_formatExtraInfo(media)}
+										${formatExtraInfo(media)}
 										</p>
 									</div>
-									<div class="media__card__info__genres-wrap">${_generateGenreElements(media.genres)}
+									<div class="media__card__info__genres-wrap">${generateGenreElements(media.genres)}
 									</div>
 									<div class="media__card__info__plot-wrap">
 										<p class="media__card__info__synopsis">
@@ -371,3 +249,135 @@ const _generateMediaCard = function (load = false) {
 			});
 	}, 1000);
 };
+
+const loadMedia = function (entries, observer) {
+	if (entries[0].isIntersecting) {
+		observer.unobserve(entries[0].target);
+		if (page <= totalPages) page += 1;
+		generateMediaCard((load = true));
+	}
+};
+
+const observer = new IntersectionObserver(loadMedia, { threshold: [0.5] });
+
+
+
+mediaDropdownBtn.addEventListener("click", () => {
+	const dropdown = document.querySelector(".type__dropdown");
+	let dropdownIcon = mediaDropdownBtn.firstElementChild;
+	closeDropdown(dropdown);
+	flipIcon(dropdownIcon);
+	dropdown.classList.toggle("dropdown--open");
+});
+
+filterFields.addEventListener("click", (e) => {
+	const filterBox = e.target.closest(".field");
+	const dropdown = e.target.closest(".field").querySelector(".filter__dropdown");
+	let dropdownIcon = filterBox.querySelector(".drop-icon");
+
+	if (!dropdown.contains(e.target)) {
+		closeDropdown(dropdown);
+		flipIcon(dropdownIcon);
+
+		dropdown.classList.toggle("dropdown--open");
+	}
+});
+
+sortBtn.addEventListener("click", (e) => {
+	const dropdown = document.querySelector(".sort__dropdown");
+	if (!dropdown.contains(e.target)) {
+		closeDropdown(dropdown);
+		dropdown.classList.toggle("dropdown--open");
+	}
+});
+
+
+
+mediaDropdown.addEventListener("click", (e) => {
+	if (e.target.classList.contains("type__dropdown__option")) {
+		mediaDropdownBtn.innerHTML = `${e.target.textContent}<i class="fi fi-rs-angle-small-down media-type__icon"></i>`;
+		mediaType = e.target.textContent.toLowerCase();
+		closeDropdown()
+		reset();
+		switchType();
+	}
+});
+
+genreDropdown.addEventListener("click", (e) => {
+	if (e.target.classList.contains("filter__dropdown__option")) {
+		if (!e.target.classList.contains("filter__dropdown__option--selected")) {
+			e.target.insertAdjacentHTML("beforeend", '<i class="fi fi-rs-check"></i>');
+			selectedGenres.add(e.target.textContent);
+		} else {
+			e.target.querySelector("i").remove();
+			selectedGenres.delete(e.target.textContent);
+		}
+		e.target.classList.toggle("filter__dropdown__option--selected");
+		showMultiTags(genreBox, selectedGenres);
+		activeFilters();
+		generateMediaCard();
+	}
+});
+
+typeDropdown.addEventListener("click", (e) => {
+	if (e.target.classList.contains("filter__dropdown__option")) {
+		if (!e.target.classList.contains("filter__dropdown__option--selected")) {
+			if (selectedType) {
+				selectedType.querySelector("i").remove();
+				selectedType.classList.remove("filter__dropdown__option--selected");
+			}
+			selectedType = e.target;
+			selectedType.insertAdjacentHTML("beforeend", '<i class="fi fi-rs-check"></i>');
+			selectedType.classList.add("filter__dropdown__option--selected");
+			typeBox.querySelector(".field__placeholder").innerText = selectedType.textContent;
+		} else {
+			e.target.querySelector("i").remove();
+			e.target.classList.remove("filter__dropdown__option--selected");
+			selectedType = null;
+			typeBox.querySelector(".field__placeholder").innerText = "Any";
+		}
+		closeDropdown();
+		activeFilters();
+		generateMediaCard();
+	}
+});
+
+statusDropdown.addEventListener("click", (e) => {
+	if (e.target.classList.contains("filter__dropdown__option")) {
+		if (!e.target.classList.contains("filter__dropdown__option--selected")) {
+			if (selectedStatus) {
+				selectedStatus.querySelector("i").remove();
+				selectedStatus.classList.remove("filter__dropdown__option--selected");
+			}
+			selectedStatus = e.target;
+			selectedStatus.insertAdjacentHTML("beforeend", '<i class="fi fi-rs-check"></i>');
+			selectedStatus.classList.add("filter__dropdown__option--selected");
+			statusBox.querySelector(".field__placeholder").innerText = selectedStatus.textContent;
+		} else {
+			e.target.querySelector("i").remove();
+			e.target.classList.remove("filter__dropdown__option--selected");
+			selectedStatus = null;
+			statusBox.querySelector(".field__placeholder").innerText = "Any";
+		}
+		closeDropdown();
+		activeFilters();
+		generateMediaCard();
+	}
+});
+
+sortDropdown.addEventListener("click", (e) => {
+	if (e.target.classList.contains("sort__dropdown__option")) {
+		activeSortTitle.dataset.sort = e.target.dataset.sort;
+		activeSortTitle.innerHTML = `<i class="fi fi-rs-sort"></i>${e.target.textContent}`;
+		closeDropdown();
+		generateMediaCard();
+	}
+});
+
+query.addEventListener("input", () => {
+	activeFilters();
+	if (query.value.length >= 3) generateMediaCard();
+});
+
+
+
