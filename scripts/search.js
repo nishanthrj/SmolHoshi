20f9e3438b5c4d8c2439a5b08caf9372d637e531
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2022-03-10 20:08:06 +0530

    Switch to Kitsu API

--- scripts/search.js
+++ scripts/search.js
@@ -22,15 +22,16 @@ const activeSortTitle = document.querySelector(".sort__method");
 const activeFilterContainer = document.querySelector(".active__filter-wrap");
 const query = document.querySelector("#query");
 
+let nextPage;
 let mediaType = mediaDropdownBtn.textContent.toLowerCase();
-let totalPages;
-let page = 1;
-
-
+const allGenres = new Set(
+	[...genreDropdown.querySelectorAll(".filter__dropdown__option")]
+		.slice(0, 16)
+		.map((g) => g.textContent)
+);
 
 const reset = function () {
-	totalPages = 0;
-	page = 1;
+	nextPage = "";
 	selectedGenres = new Set();
 	selectedType = null;
 	selectedStatus = null;
@@ -62,16 +63,17 @@ const switchType = function () {
 	if (mediaType === "manga") {
 		typeDropdown.innerHTML = `<ul class="filter__dropdown__list">
 			<li class="filter__dropdown__option">Manga</li>
-			<li class="filter__dropdown__option">Lightnovel</li>
-			<li class="filter__dropdown__option">Oneshot</li>
+			<li class="filter__dropdown__option">Novel</li>
 			<li class="filter__dropdown__option">Manhwa</li>
 			<li class="filter__dropdown__option">Manhua</li>
+			<li class="filter__dropdown__option">Doujin</li>
 		</ul>`;
 
 		statusDropdown.innerHTML = `<ul class="filter__dropdown__list">
-				<li class="filter__dropdown__option">Publishing</li>
-				<li class="filter__dropdown__option">Complete</li>
+				<li class="filter__dropdown__option">Current</li>
+				<li class="filter__dropdown__option">Finished</li>
 				<li class="filter__dropdown__option">Upcoming</li>
+				<li class="filter__dropdown__option">Unreleased</li>
 			</ul>`;
 	} else {
 		typeDropdown.innerHTML = `<ul class="filter__dropdown__list">
@@ -83,9 +85,10 @@ const switchType = function () {
 		</ul>`;
 
 		statusDropdown.innerHTML = `<ul class="filter__dropdown__list">
-				<li class="filter__dropdown__option">Airing</li>
-				<li class="filter__dropdown__option">Complete</li>
-				<li class="filter__dropdown__option">Upcoming</li>
+			<li class="filter__dropdown__option">Current</li>
+			<li class="filter__dropdown__option">Finished</li>
+			<li class="filter__dropdown__option">Upcoming</li>
+			<li class="filter__dropdown__option">Unreleased</li>
 			</ul>`;
 	}
 };
@@ -132,27 +135,27 @@ const getUserQuery = function () {
 
 	return {
 		q: query.value ? query.value : "",
-		genres: genres.length > 0 ? [...genres].map((g) => g.dataset.genreId) : [],
+		genres: genres.length > 0 ? [...genres].map((g) => g.textContent) : [],
 		type: type ? type.textContent : "",
 		status: status ? status.textContent : "",
-		order: activeSortTitle.dataset.sort,
-		sort: activeSortTitle.dataset.sort === "title" ? "asc" : "desc",
+		sort: activeSortTitle.dataset.sort,
 	};
 };
 
 const activeFilters = function () {
 	let activeFilters = "";
 	const filters = getUserQuery();
-	const genresNames = genreDropdown.querySelectorAll(".filter__dropdown__option--selected");
 
-	if (filters.q) activeFilters += `<div class="active__filter__item">Search: ${filters.q}</div>\n`;
-	if (genresNames.length > 0) {
-		genresNames.forEach((g) => {
-			activeFilters += `<div class="active__filter__item">${g.textContent}</div>\n`;
+	if (filters.q)
+		activeFilters += `<div class="active__filter__item">Search: ${filters.q}</div>\n`;
+	if (filters.genres.length > 0) {
+		filters.genres.forEach((g) => {
+			activeFilters += `<div class="active__filter__item">${g}</div>\n`;
 		});
 	}
 	if (filters.type) activeFilters += `<div class="active__filter__item">${filters.type}</div>\n`;
-	if (filters.status) activeFilters += `<div class="active__filter__item">${filters.status}</div>\n`;
+	if (filters.status)
+		activeFilters += `<div class="active__filter__item">${filters.status}</div>\n`;
 
 	if (activeFilters) {
 		activeFilterContainer.innerHTML = activeFilters;
@@ -163,86 +166,116 @@ const activeFilters = function () {
 
 const ratingIcon = function (rating) {
 	if (!rating) return "";
-	else if (rating <= 4) return `<i class="fi fi-rs-sad"></i>`;
-	else if (rating >= 7) return `<i class="fi fi-rs-smile"></i>`;
+	else if (rating <= 45) return `<i class="fi fi-rs-sad"></i>`;
+	else if (rating >= 75) return `<i class="fi fi-rs-smile"></i>`;
 	else return `<i class="fi fi-rs-meh"></i>`;
 };
 
-const generateGenreElements = function (genres) {
+const generateGenreElements = function (listGenres, mediaGenres) {
 	let tags = "";
-	genres.slice(0, 5).forEach((g) => {
-		tags += `<span class="media__card__info__genre">${g.name}</span>\n`;
-	});
+	let tagCount = 0;
+
+	for (let mg of mediaGenres) {
+		for (let lg of listGenres) {
+			if (mg && mg.id === lg.id) {
+				if (allGenres.has(lg.attributes.title)) {
+					tags += `<span class="media__card__info__genre">${lg.attributes.title}</span>\n`;
+					tagCount++;
+					break;
+				}
+			}
+		}
+		if (tagCount >= 4) return tags;
+	}
 	return tags;
 };
 
-const formatScoreInfo = function (media) {
-	if (mediaType === "manga") {
-		return media.scored;
-	}
-	return media.score;
-};
-
 const formatExtraInfo = function (media) {
-	if (mediaType === "manga") {
+	if (media.subtype === "TV") {
 		return (
-			`${media.chapters ? " • " + media.chapters + " Chapter" : ""}` +
-			`${media.chapters > 1 ? "s" : ""}`
+			`${media.episodeCount ? " • " + media.episodeCount + " Episode" : ""}` +
+			`${media.episodeCount > 1 ? "s" : ""}`
+		);
+	} else if (media.subtype === "movie") {
+		let hour = Math.floor(media.totalLength / 60);
+		let minutes = Math.floor(media.totalLength % 60);
+		return (
+			`${hour || minutes ? " • " : ""}` +
+			`${hour ? hour + " Hour" : ""}` +
+			`${hour > 1 ? "s" : ""}` +
+			`${minutes ? " " + minutes + " Minute" : ""}` +
+			`${minutes > 1 ? "s" : ""}`
 		);
 	}
 	return (
-		`${media.episodes ? " • " + media.episodes + " Episode" : ""}` +
-		`${media.episodes > 1 ? "s" : ""}`
+		`${media.chapterCount ? " • " + media.chapterCount + " Chapter" : ""}` +
+		`${media.chapterCount > 1 ? "s" : ""}`
 	);
+};
+
+const constructUrl = function () {
+	const query = getUserQuery();
+	let base = `https://kitsu.io/api/edge/${mediaType}?include=categories&page[limit]=20&`;
+
+	if (query.q) base += `filter[text]=${query.q}&`;
+	if (query.type) base += `filter[subtype]=${query.type}&`;
+	if (query.status) base += `filter[status]=${query.status}&`;
+	if (query.genres.length) base += `filter[categories]=${[...query.genres]}&`;
+	if (!query.q) base += `sort=${query.sort}`; //FIXME: Sort by "Title" doesn't work when filters are applied
+	return base;
 };
 
 const generateMediaCard = function (load = false) {
 	setTimeout(() => {
+		let url;
 		let cards = "";
 		const container = document.querySelector(".results");
-		const query = getUserQuery();
-		const url =
-			`https://api.jikan.moe/v4/${mediaType}?q=${query.q}&page=${page}&` +
-			`type=${query.type}&status=${query.status}&sfw=true&genres_exclude=12,49` +
-			`&genres=${[...query.genres]}&order_by=${query.order}&sort=${query.sort}`;
+
+		if (load) url = nextPage;
+		else url = constructUrl();
 
 		fetch(url)
 			.then((res) => res.json())
 			.then((data) => {
-				totalPages = data.pagination.last_visible_page;
-				data.data.forEach((media) => {
+				data.data.forEach((x) => {
+					media = x.attributes;
 					cards += `<div class="media__card">
 								<div class="media__card__cover">
-									<img src="${media.images.jpg.large_image_url}" alt="">
+									<img src="${media.posterImage.original}" alt="">
 								</div>
 								<div class="media__card__info">
 									<div class="media__card__info__header-wrap">
-										<a href="#" class="media__card__info__title">${media.title}</a>
-										<p class="media__card__info__score">${ratingIcon(formatScoreInfo(media))}
-										${formatScoreInfo(media) ? formatScoreInfo(media).toString().padEnd(4, 0) : ""}</p>
+										<a href="#" class="media__card__info__title">${media.canonicalTitle}</a>
+										<p class="media__card__info__score">${ratingIcon(Number(media.averageRating))}
+										${media.averageRating ? Math.round(Number(media.averageRating)) + '%' : ""}</p>
 									</div>
 									<div class="media__card__info__extra-wrap">
 										<p class="media__card__info__extra">
-										${media.type}
+										${media.subtype}
 										${formatExtraInfo(media)}
 										</p>
 									</div>
-									<div class="media__card__info__genres-wrap">${generateGenreElements(media.genres)}
+									<div class="media__card__info__genres-wrap">${generateGenreElements(
+										data.included,
+										x.relationships.categories.data
+									)}
 									</div>
 									<div class="media__card__info__plot-wrap">
 										<p class="media__card__info__synopsis">
-										${media.synopsis ? media.synopsis.replace("[Written by MAL Rewrite]", "") : ""}
+										${media.synopsis ? media.synopsis : ""}
 										</p>
 									</div>
 								</div>
 							</div>`;
 				});
+
 				if (load) {
 					container.insertAdjacentHTML("beforeend", cards);
 				} else {
 					container.innerHTML = cards;
-					page = 1;
 				}
+
+				nextPage = data.links.next ? data.links.next : null;
 
 				let loadedCards = container.children;
 				observer.observe(loadedCards.item(loadedCards.length - 4));
@@ -253,14 +286,11 @@ const generateMediaCard = function (load = false) {
 const loadMedia = function (entries, observer) {
 	if (entries[0].isIntersecting) {
 		observer.unobserve(entries[0].target);
-		if (page <= totalPages) page += 1;
-		generateMediaCard((load = true));
+		if (nextPage) generateMediaCard((load = true));
 	}
 };
 
 const observer = new IntersectionObserver(loadMedia, { threshold: [0.5] });
-
-
 
 mediaDropdownBtn.addEventListener("click", () => {
 	const dropdown = document.querySelector(".type__dropdown");
@@ -291,13 +321,11 @@ sortBtn.addEventListener("click", (e) => {
 	}
 });
 
-
-
 mediaDropdown.addEventListener("click", (e) => {
 	if (e.target.classList.contains("type__dropdown__option")) {
 		mediaDropdownBtn.innerHTML = `${e.target.textContent}<i class="fi fi-rs-angle-small-down media-type__icon"></i>`;
 		mediaType = e.target.textContent.toLowerCase();
-		closeDropdown()
+		closeDropdown();
 		reset();
 		switchType();
 	}
@@ -378,6 +406,3 @@ query.addEventListener("input", () => {
 	activeFilters();
 	if (query.value.length >= 3) generateMediaCard();
 });
-
-
-
