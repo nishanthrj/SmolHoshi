75ee54aa4cd85c52fd58880a5ac69050192a7427
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2022-03-20 19:28:42 +0530

    Implement anime view

--- base/views.py
+++ base/views.py
@@ -6,3 +6,11 @@ def home(request):
 
 def search(request):
     return render(request, 'search.html')
+
+def anime(request, mediaId, slug):
+    title = slug.replace('-', ' ').title()
+    context = {
+        'animeId': mediaId,
+        'title': title
+    }
+    return render(request, 'anime.html', context=context)
