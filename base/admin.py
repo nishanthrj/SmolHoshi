97d0fc1f484fee9a73d9d63eefaacb4514f2733e
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2022-04-16 15:21:49 +0530

    Create recommendation model

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import AnimeRecommendation, MangaRecommendation
 
 # Register your models here.
+admin.site.register(AnimeRecommendation)
+admin.site.register(MangaRecommendation)
+
--- base/models.py
+++ base/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+from djongo import models
 
 # Create your models here.
+
+class Media(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+    type = models.CharField(max_length=25)
+    poster = models.CharField(max_length=200)
+    
+    class Meta:
+        managed = False
+    
+class AnimeRecommendation(models.Model):
+    id = models.IntegerField(primary_key=True)
+    data = models.ArrayField(model_container=Media)
+    objects = models.DjongoManager()
+    
+    class Meta:
+        db_table = "anime_recommendation"
+        
+        
+class MangaRecommendation(models.Model):
+    id = models.IntegerField(primary_key=True)
+    data = models.ArrayField(model_container=Media)
+    objects = models.DjongoManager()
+    
+    class Meta:
+        db_table = "manga_recommendation"
